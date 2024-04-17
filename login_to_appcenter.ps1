035cdcde4dc4da30936761c9d53a8af631624c1c
adwaitpawar <110279221+adwaitpawar@users.noreply.github.com>
2024-04-17 10:27:22 +0530

    Create login_to_appcenter.ps1

---
+++ login_to_appcenter.ps1
@@ -0,0 +1,11 @@
+# App Center CLI version
+$APPCENTER_CLI_VERSION = "1.2.2"
+
+# App Center token
+$APPCENTER_TOKEN = "555da71027982f713d2bdbd0a04222d8c891348c"  # Replace with your actual token
+
+# Install App Center CLI
+npm install -g appcenter-cli@$APPCENTER_CLI_VERSION
+
+# Login to App Center using the provided token
+appcenter login --token $APPCENTER_TOKEN
